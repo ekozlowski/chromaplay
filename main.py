09ebf40e54f23ebee15e3b1b829a61353cdbc755
Ed Kozlowski <ekozlowski1@gmail.com>
2021-02-13 13:41:10 -0600

    Threading and ctrl-c escaping working - but there's a better way to do it than using so many globals.  :/

--- main.py
+++ main.py
@@ -2,21 +2,36 @@ from loguru import logger
 import time
 import requests
 import json
+import threading
 
 
 class APIException(Exception):
     """Raised when there was an error returned from the Chroma API call."""
     pass
 
+LAST_API_REQUEST = None
+RUNNING = True
+
+def keepalive(chroma_obj):
+    global RUNNING, LAST_API_REQUEST
+    # while we're running, if the last API request is more than 5 seconds 
+    # old, make a keepalive API request, until RUNNING is False.
+    while RUNNING:
+        if time.time() - LAST_API_REQUEST > 5:
+            chroma_obj.heartbeat()
+        else:
+            time.sleep(1)
 
 class Chroma:
     def __init__(self):
+        global RUNNING, LAST_API_REQUEST
         self.headers = {
             "content-type": "application/json"
         }
         self.uri = None
         data = json.load(open('chroma_data_post.json', 'r'))
         response = requests.post("http://localhost:54235/razer/chromasdk", json=data, headers=self.headers)
+        LAST_API_REQUEST = time.time()
         logger.debug(response)
         logger.debug(response.text)
         if response.status_code == 200:
@@ -25,18 +40,29 @@ class Chroma:
             self.sessionid = data.get('sessionid')
         else:
             raise APIException("Problem initializing API connection.  Response code: {response.status_code}\nText: {response.text}")
-    
+
+
+    def __del__(self):
+        response = requests.delete(self.uri)
+        logger.debug(response)
+        logger.debug(response.text)
+
+
     def keyboard_post(self, data):
+        global LAST_API_REQUEST
         response = requests.post(self.uri + "/keyboard", json=data, headers=self.headers)
+        LAST_API_REQUEST = time.time()
         logger.debug(response)
         logger.debug(response.text)
         return response
 
     def apply_effect(self, effect_id):
+        global LAST_API_REQUEST
         data = {
             "id": f"{effect_id}"
         }
         response = requests.put(self.uri + "/effect", json=data, headers=self.headers)
+        LAST_API_REQUEST = time.time()
         logger.debug(f"Response: {response}")
         return response
 
@@ -77,14 +103,15 @@ class Chroma:
 
 
     def heartbeat(self):
+        global LAST_API_REQUEST
         response = requests.put(self.uri + "/heartbeat", json={}, headers=self.headers)
+        LAST_API_REQUEST = time.time()
         logger.debug(response)
         logger.debug(response.text)
         
 
-def flashy_green_red_blue_keyboard():
+def flashy_green_red_blue_keyboard(c):
     times = 10
-    c = Chroma()
     red_effect = c.create_static_effect(c.get_color('red'))
     green_effect = c.create_static_effect(c.get_color('green'))
     blue_effect = c.create_static_effect(c.get_color('blue'))
@@ -92,14 +119,30 @@ def flashy_green_red_blue_keyboard():
     for _ in range(1):
         #effect_id = create_static_effect(65280) # Green
         time.sleep(3)
-        
-        while(True):
+        for x in range(10):
             c.apply_effect(red_effect)
             time.sleep(.1)
             c.apply_effect(green_effect)
             time.sleep(.1)
             c.apply_effect(blue_effect)
             time.sleep(.1)
+        time.sleep(20)
+        for x in range(10):
+            c.apply_effect(red_effect)
+            time.sleep(.1)
+            c.apply_effect(green_effect)
+            time.sleep(.1)
+            c.apply_effect(blue_effect)
+            time.sleep(.1)
+        time.sleep(20)
+        for x in range(10):
+            c.apply_effect(red_effect)
+            time.sleep(.1)
+            c.apply_effect(green_effect)
+            time.sleep(.1)
+            c.apply_effect(blue_effect)
+            time.sleep(.1)
+        
 
 # To program keyboard effects, We program 21 columns, 6 rows, and give each
 # row a Chroma color, or a 0 to indicate "off".
@@ -134,8 +177,19 @@ def create_checkerboard_keyboard():
 
 
 if __name__ == "__main__":
-    flashy_green_red_blue_keyboard()
-
+    # create a chroma obj.
+    c = Chroma()
+    # spinoff a thread to keep our Chroma object alive.
+    keepalive_thread = threading.Thread(target=keepalive, args=(c,))
+    keepalive_thread.start()
+    try:
+        flashy_green_red_blue_keyboard(c)
+    except KeyboardInterrupt:
+        pass
+    RUNNING = False
+    keepalive_thread.join()
+    del(c)
+    
     #effect = create_checkerboard_keyboard()
     #time.sleep(2)
     #apply_effect(effect)
